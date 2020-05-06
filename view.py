c4e90c4f0b218c711ab8c404b805b80957df4586
Onore849 <rion3376@gmail.com>
2020-05-06 20:29:24 +0900

    create add_employee

--- view.py
+++ view.py
@@ -9,10 +9,40 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+# 従業員の一覧
 @app.route('/')
 def index():
     connection = get_db_connection()
     cursor = connection.cursor()
-    res = cursor.ececute('SELECT * FROM employees_list')
+    res = cursor.execute('SELECT * FROM employees_list')
     return render_template('index.html', employees_list=res.fetchall())
 
+# 従業員の追加
+@app.route('/add', methods=['GET', 'POST'])
+def add_employee():
+    if request.method == 'GET':
+        employee = {}
+        return render_template('add_employee.html', employee=employee)
+    else:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        error = []
+
+        if not request.form['name']:
+            error.append('従業員名を追加してください')
+        
+        if error:
+            employee = request.form.to_dict()
+            # return render_template('edit.html', employee=employee, error_list=error)
+            return render_template('add_employee.html', employee=employee, error_list=error)
+
+        cursor.execute('INSERT INTO employees_list(name, comment) VALUES(?, ?)',
+                        (request.form['name'],
+                        request.form['comment'])
+                        )
+        
+        connection.commit()
+        return redirect(url_for('index'))
+        
+if __name__ == '__main__':
+    app.run(debug=True)
